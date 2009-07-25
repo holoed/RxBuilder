983a390aec748749950c70cac23a8d81d0e85256
holoed_cp <holoed@users.noreply.github.com>
2009-07-25 23:04:43 +0000

    Added F# implementation of GroupBy combinator + Tests

---
+++ ReactiveLinq/CSReactiveLinq/LinqEnabler.cs
@@ -0,0 +1,33 @@
+#region License
+
+/* ****************************************************************************
+ * Copyright (c) Edmondo Pentangelo. 
+ *
+ * This source code is subject to terms and conditions of the Microsoft Public License. 
+ * A copy of the license can be found in the License.html file at the root of this distribution. 
+ * By using this source code in any fashion, you are agreeing to be bound by the terms of the 
+ * Microsoft Public License.
+ *
+ * You must not remove this notice, or any other, from this software.
+ * ***************************************************************************/
+
+#endregion
+
+using System;
+using ReactiveLinq;
+
+namespace CSReactiveLinq
+{
+    public static class LinqEnabler
+    {
+        public static IObservable<U> Select<T, U>(this IObservable<T> p, Func<T, U> selector)
+        {
+            return new ObservableWrapper<T, U>(p, selector);
+        }
+
+        public static IObservable<V> SelectMany<T, U, V>(this IObservable<T> p, Func<T, IObservable<U>> selector, Func<T, U, V> projector)
+        {
+            return new ObservableWrapper<T, U, V>(p, selector, projector);
+        }
+    }
+}
---
+++ ReactiveLinq/ReactiveLinq.Tests/ReactiveLinqTests.cs
@@ -0,0 +1,40 @@
+﻿#region License
+
+/* ****************************************************************************
+ * Copyright (c) Edmondo Pentangelo. 
+ *
+ * This source code is subject to terms and conditions of the Microsoft Public License. 
+ * A copy of the license can be found in the License.html file at the root of this distribution. 
+ * By using this source code in any fashion, you are agreeing to be bound by the terms of the 
+ * Microsoft Public License.
+ *
+ * You must not remove this notice, or any other, from this software.
+ * ***************************************************************************/
+
+#endregion
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using ReactiveLinq.Agents;
+
+namespace ReactiveLinq
+{
+    public abstract class ReactiveLinqTests
+    {
+        protected SpotAgent spots = new SpotAgent();
+        protected RiskAgent risks = new RiskAgent();
+
+        public abstract void Select();
+        public abstract void SelectMany();
+        public abstract void GroupBy();
+
+        protected List<T> newListOfType<T>(IObservable<T> q)
+        {
+            return new List<T>();
+        }
+
+    }
+}
