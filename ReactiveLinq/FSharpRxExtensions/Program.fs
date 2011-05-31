0d5ecc5d0f53456f27b95ebe3e1cfff12c19c42a
holoed_cp <SND\holoed_cp>
2011-05-31 23:33:33 +0000

    Experiments in F# Computation Expressions and Reactive Extensions

---
+++ ReactiveLinq/FSharpRxExtensions/Program.fs
@@ -0,0 +1,22 @@
+﻿open System
+open System.Linq
+open System.Reactive.Linq
+open System.Reactive.Concurrency
+open RxLib
+
+
+type rxBuilder() =    
+   member this.Delay f = ObservableExt.Defer f
+   member this.Combine (xs:'a IObservable, ys: 'a IObservable) =
+               ObservableExt.Combine (xs, ys)
+   member this.Yield x = Observable.Return x
+   member this.YieldFrom xs = xs
+
+let rx = rxBuilder()
+
+let rec f x = rx { yield x 
+                   yield! f (x + 1) }
+
+do f 5 |> fun xs -> Observable.ObserveOn(xs, Scheduler.CurrentThread) |> Observable.subscribe (fun x -> printfn "%A" x) |> ignore
+
+do System.Console.ReadLine() |> ignore
