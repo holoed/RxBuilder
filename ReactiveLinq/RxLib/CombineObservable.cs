91b90bdf54f65e08dbed2ce2819c51fc740085cc
holoed_cp <holoed@users.noreply.github.com>
2011-05-31 23:33:33 +0000

    Experiments in F# Computation Expressions and Reactive Extensions

---
+++ ReactiveLinq/RxLib/CombineObservable.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Reactive.Disposables;
+using System.Reactive.Concurrency;
+using System.Threading;
+
+namespace RxLib
+{
+    public class CombineObservable<T> : IObservable<T>
+    {
+        private IObservable<T> _xs;
+        private IObservable<T> _ys;
+
+        public CombineObservable(IObservable<T> xs, IObservable<T> ys)
+        {
+            _xs = xs;
+            _ys = ys;
+        }
+
+        public IDisposable Subscribe(IObserver<T> observer)
+        {
+            var mutableDisposable = new MutableDisposable();
+            var handle = new ManualResetEvent(false);
+            Scheduler.ThreadPool.Schedule(() => ScheduleNextObservable(handle, mutableDisposable, observer));
+            mutableDisposable.Disposable = _xs.Subscribe(observer.OnNext, observer.OnError, () => handle.Set());
+            return mutableDisposable;
+        }
+
+        private void ScheduleNextObservable(ManualResetEvent handle, MutableDisposable mutableDisposable, IObserver<T> observer)
+        {
+
+            handle.WaitOne();
+            var deferObservable = (DeferObservable<T>)_ys;
+            var innerObservable = deferObservable.GetInner();
+            while (innerObservable is DeferObservable<T>)
+                innerObservable = ((DeferObservable<T>)innerObservable).GetInner();
+            var combineObservable = ((CombineObservable<T>)innerObservable);
+            _xs = combineObservable.GetLeftObservable();
+            _ys = combineObservable.GetRightObservable();
+            mutableDisposable.Disposable = Subscribe(observer);
+        }
+
+        private IObservable<T> GetLeftObservable()
+        {
+            return _xs;
+        }
+
+        private IObservable<T> GetRightObservable()
+        {
+            return _ys;
+        }
+    }
+}
---
+++ ReactiveLinq/RxLib/DeferObservable.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace RxLib
+{
+    public class DeferObservable<T> : IObservable<T>
+    {
+        private readonly Func<IObservable<T>> _func;
+
+        public DeferObservable(Func<IObservable<T>> func)
+        {
+            _func = func;
+        }
+
+        public IDisposable Subscribe(IObserver<T> observer)
+        {
+            return _func().Subscribe(observer);
+        }
+
+        public IObservable<T> GetInner()
+        {
+            return _func();
+        }
+    }
+}
