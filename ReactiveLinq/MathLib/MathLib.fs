3a9ff2d29a24fc7144c571b697e2a299031dd94f
holoed_cp <holoed@users.noreply.github.com>
2011-09-08 20:28:42 +0000

    Checked in F# WPF Geometry2D Experiments

---
+++ ReactiveLinq/MathLib/MathLib.fs
@@ -0,0 +1,21 @@
+﻿module MathLib
+
+open System
+
+[<Measure>]
+type rad
+
+[<Measure>]
+type deg
+
+let pi = Math.PI * 1.<rad>
+
+let sqrt = Math.Sqrt
+
+let sin (x:float<rad>) = Math.Sin (x / 1.<rad>)
+
+let cos (x:float<rad>) = Math.Cos (x / 1.<rad>)
+
+let atan2 x y = Math.Atan2(x, y) * 1.<rad>
+
+let toRad (a:float<deg>) = (a / 180.0<deg>) * pi
