8d3d132c4d176a659880a224d9b6f464188fb7fc
holoed_cp <SND\holoed_cp>
2011-09-08 20:28:42 +0000

    Checked in F# WPF Geometry2D Experiments

---
+++ ReactiveLinq/MathLib/Geometry2DLib.fs
@@ -0,0 +1,67 @@
+﻿module Geometry2DLib
+
+open System
+open MathLib
+
+type Vector = 
+        private { x:float; y:float } 
+        static member (+) (v1, v2) = { x = v1.x + v2.x; y = v1.y + v2.y }
+        static member (-) (v1, v2) = { x = v1.x - v2.x; y = v1.y - v2.y }
+        static member (*) (k, v) = { x = v.x * k; y = v.y * k }
+        static member (*) (v, k) = { x = v.x * k; y = v.y * k }
+
+let vector x y = { x = x; y = y }
+
+let magnitude v = sqrt (v.x * v.x + v.y * v.y)
+
+let angle v = atan2 v.x v.y
+
+let normalize v = let size = magnitude v
+                  { x = v.x / size; y = v.y / size }
+
+let dot v1 v2 = let mv1 = magnitude v1
+                let mv2 = magnitude v2
+                let ang1 = angle v1
+                let ang2 = angle v2
+                let dang = if (ang1 > ang2) then ang1 - ang2 
+                                            else ang2 - ang1
+                mv1 * mv2 * cos dang
+
+let fromPolar rho theta = { x = rho * cos theta
+                            y = rho * sin theta }
+
+let getX v = v.x
+
+let getY v = v.y
+                
+type Segment = private { p1: Vector; p2 : Vector }
+
+let segment p1 p2 = { p1 = p1; p2 = p2 }
+
+let getP1 s = s.p1
+
+let getP2 s = s.p2
+
+let normals s = let dv = s.p2 - s.p1
+                ({ x = -dv.y; y = dv.x}, { x = dv.y; y = -dv.x })
+
+let midpoint s = { x = (s.p1.x + s.p2.x) / 2.0
+                   y = (s.p1.y + s.p2.y) / 2.0 }
+
+let bounce v n = let n = normalize n
+                 let d = dot n v
+                 - 2.0 * (d * n) + v
+
+let getP1X = getP1 >> getX
+
+let getP1Y = getP1 >> getY
+
+let getP2X = getP2 >> getX
+
+let getP2Y = getP2 >> getY
+
+
+
+
+
+
---
+++ ReactiveLinq/SampleUsage/Program.fs
@@ -0,0 +1,59 @@
+﻿
+// Learn more about F# at http://fsharp.net
+// F# for WPF project template at http://code.msdn.com/fs
+// Template Version 0.1 Preview
+open System
+open System.Windows
+open System.Windows.Controls
+open System.Windows.Shapes
+open System.Windows.Media
+open Petzold.Media2D
+open MathLib
+open Geometry2DLib
+
+let canvas = new Canvas()
+
+let drawLine segment color = canvas.Children.Add (new Line (X1 = getP1X segment, 
+                                                            Y1 = getP1Y segment, 
+                                                            X2 = getP2X segment, 
+                                                            Y2 = getP2Y segment, 
+                                                            Stroke = color, 
+                                                            StrokeThickness = 5.0 )) |> ignore
+
+let drawArrow segment color = canvas.Children.Add (new ArrowLine (X1 = getP1X segment, 
+                                                                  Y1 = getP1Y segment, 
+                                                                  X2 = getP2X segment, 
+                                                                  Y2 = getP2Y segment, 
+                                                                  Stroke = color, 
+                                                                  StrokeThickness = 5.0 )) |> ignore
+
+let s = segment (vector 100.0 150.0) (vector 200.0 200.0)
+
+let mp = midpoint s
+
+let (v1, v2) = normals s
+
+let velocity = fromPolar 100.0 (toRad -30.0<deg>) 
+
+let vs1 = segment mp (v1 + mp)
+
+let vs2 = segment mp (v2 + mp)
+
+let velocityArrow = segment (mp - velocity) mp 
+
+let bounceVel = bounce velocity v2
+
+let bvs = segment mp (mp +  bounceVel)
+
+drawLine s (Brushes.Red)
+
+drawArrow vs1 (Brushes.Blue)
+
+drawArrow vs2 (Brushes.Green)
+
+drawArrow velocityArrow (Brushes.Black)
+
+drawArrow bvs (Brushes.Coral)
+ 
+let window = new Window(Title = "WpfApplication1", Content = canvas)
+[<STAThread>] ignore <| (new Application()).Run window
