775acfe28f9086b04c1a5de6de3c30c983e4666b
holoed_cp <holoed@users.noreply.github.com>
2011-09-09 00:35:13 +0000

    Added more geometric operations

---
+++ ReactiveLinq/RxLib/RxLib.fs
@@ -0,0 +1,36 @@
+﻿module RxLib
+
+open System
+open System.Linq
+open System.Reactive.Linq
+
+type rxBuilder() =    
+    member this.Bind ((xs:'a IObservable), (f:'a -> 'b IObservable)) =
+        Observable.SelectMany (xs, f)
+    member this.Delay f = Observable.Defer f
+    member this.Return x = Observable.Return x
+    member this.ReturnFrom xs = xs
+    member this.Combine (xs:'a IObservable, ys: 'a IObservable) =
+        Observable.Concat (xs, ys)
+    member this.For (xs : 'a seq, f: 'a -> 'b IObservable) =
+        Observable.For(xs, new Func<_, IObservable<_>>(f)) 
+    member this.TryFinally (xs: 'a IObservable, f : unit -> unit) =
+        Observable.Finally(xs, new Action(f))
+    member this.TryWith (xs: 'a IObservable, f: exn -> 'a IObservable) =
+        Observable.Catch (xs, new Func<exn, 'a IObservable>(f))
+    member this.While (f, xs: 'a IObservable) =
+        Observable.While (new Func<bool>(f), xs)
+    member this.Yield x = Observable.Return x
+    member this.YieldFrom xs = xs
+    member this.Zero () = Observable.Empty()
+              
+let rx = rxBuilder()
+
+// Rx combinators
+
+let repeat (xs:IObservable<_>) = xs.Repeat()
+
+let timer (dueTime:TimeSpan) (period:TimeSpan) = Observable.Timer (dueTime, period)
+
+let startWith = Observable.StartWith
+
---
+++ ReactiveLinq/RxWorldCSharp/MainWindow.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.ComponentModel;
+using System.Reactive.Linq;
+using System.Windows;
+
+namespace RxWorldCSharp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            RxWorld.Run(() => Tuple.Create(canvasWorld.ActualWidth, canvasWorld.ActualHeight), ConvertToViewModel).ObserveOnDispatcher().Subscribe(UpdateView());
+        }
+
+        private Action<World> UpdateView()
+        {
+            return x =>
+                       {
+                           if (DataContext == null)
+                               DataContext = x;
+                           else 
+                               UpdateViewModel((World) DataContext, x);
+                       };
+        }
+
+        private static void UpdateViewModel(World current, World newWorld)
+        {
+            current.Robot.Size = newWorld.Robot.Size;
+            current.Robot.Position = newWorld.Robot.Position;                                    
+        }
+
+        private static World ConvertToViewModel(RxWorld.World arg)
+        {
+            return new World
+                       {
+                           Robot = new Robot
+                                       {
+                                           Size = new Size(arg.robot.size.Item1, arg.robot.size.Item2),
+                                           Position = new Point(arg.robot.pos.Item1, arg.robot.pos.Item2)                                           
+                                       }
+                       };
+        }
+    }
+
+    public class Robot : INotifyPropertyChanged
+    {
+        private Size _size;
+        public Size Size
+        {
+            get { return _size; }
+            set {if (value == _size) return;
+                  _size = value;
+                  OnPropertyChanged("Size");}
+        }
+
+        private Point _position;
+        public Point Position
+        {
+            get { return _position; }
+            set {if (value == _position) return;
+                _position = value;
+                  OnPropertyChanged("Position");}
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string name)
+        {
+            var handler = PropertyChanged;
+            if (handler != null) handler(this, new PropertyChangedEventArgs(name));
+        }
+    }
+
+    public class World 
+    {     
+        public Robot Robot { get; set; }           
+    }
+}
